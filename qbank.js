2b0bd9b8e7b01b2f65e35a145f47d8b37e50e075
Srini Sekhar <sekhar4web@gmail.com>
2013-06-03 17:21:49 +0530

    Create qbank.js
    A sample question bank file

---
+++ qbank.js
@@ -0,0 +1,120 @@
+var qbank  = {};
+
+qbank['js']	= [];
+qbank['html']	= [];
+
+qbank['js'].push({
+	level:	'basic',
+	questions:	[
+		{
+			title:	'One JS BASIC A question statement?',
+			type:	'single',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans: 	[1],
+			comments:	'If there is a wrong answer, a justification is needed'
+			
+		},
+		{
+			title:	'Two JS BASIC A question statement?',
+			type:	'multi',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans:	[1,3],
+			comments:	'If there is a wrong answer, a justification is needed'
+		}
+	]
+});
+
+qbank['js'].push({
+	level:	'basic',
+	questions:	[
+		{
+			title:	'Three JS BASIC A question statement?',
+			type:	'single',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans: 	[1],
+			comments:	'If there is a wrong answer, a justification is needed'
+			
+		},
+		{
+			title:	'Four JS BASIC A question statement?',
+			type:	'multi',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans:	[1,3],
+			comments:	'If there is a wrong answer, a justification is needed'
+		}
+	]
+});
+
+qbank['js'].push({
+	level:	'intermediate',
+	questions:	[
+		{
+			title:	'One JS Intermediate  A question statement?',
+			type:	'single',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans: 	[1],
+			comments:	'If there is a wrong answer, a justification is needed'
+			
+		},
+		{
+			title:	'Two JS Intermediate A question statement?',
+			type:	'multi',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans:	[1,3],
+			comments:	'If there is a wrong answer, a justification is needed'
+		}
+	]
+});
+
+qbank['html'].push({
+	level:	'basic',
+	questions:	[
+		{
+			title:	'One HTML Basic A question statement?',
+			type:	'single',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans: 	[1],
+			comments:	'If there is a wrong answer, a justification is needed'
+			
+		},
+		{
+			title:	'One HTML Basic A question statement?',
+			type:	'multi',						
+			anss:	[
+				'Answer 1',
+				'Answer 2',
+				'Answer 3'
+			],
+			ans:	[1,3],
+			comments:	'If there is a wrong answer, a justification is needed'
+		}
+	]
+});
